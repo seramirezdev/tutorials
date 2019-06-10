bdd990638c7bad4e409f9c93fa10ae356e9b0844
Sergio Ramírez <seramirezdev@gmail.com>
2019-06-09 22:14:40 -0500

    Flutter provider whitout get_it

---
+++ flutter_provider_no_get_it/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_provider/core/models/user.dart';
+import 'package:flutter_provider/core/services/authentication_service.dart';
+import 'package:flutter_provider/locator.dart';
+import 'package:flutter_provider/ui/router.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  setupLocator();
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.controller(
+      initialData: User.initial(),
+      builder: (context) => locator<AuthenticationService>().userController,
+      child: MaterialApp(
+        title: 'Flutter Provider',
+        theme: ThemeData(),
+        initialRoute: Router.initialRoute,
+        onGenerateRoute: Router.generateRoute,
+      ),
+    );
+  }
+}
